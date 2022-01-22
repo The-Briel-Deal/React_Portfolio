f552779efd17b9f2bf4747a100262922900a4dbb
Gabriel Ford <gabe@fordltc.net>
2022-01-22 07:12:08 -0800

    modularized the second page section a bit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDom from "react-dom";
 import nav from "./nav";
-import { DrawMain } from "./components";
+import { DrawMain, SectionTwo } from "./components";
 import FA from 'react-fontawesome'
+import './index.css'
+
+// CONTENT
+const titleText = <span>My name is Gabriel.</span>
+const subtitleText = <span>I'm a <strong>software engineer</strong> that loves building things to make life better.</span>
+const ghLink = <a href="https://github.com/The-Briel-Deal" className="social_icon"><FA name="fab fa-github-square" /></a>
+const liLink = <a href="https://www.linkedin.com/in/gabrielford/" className="social_icon"><FA name="fab fa-linkedin-square" /></a>
+const socialBlock = <div className="column">{ghLink}{liLink}</div>
+const avatar = <img id="avatar_of_me" src={require('./avatar.png')} alt="Avatar of me" />
 
 ReactDom.render(
     <div>
         {nav}
         <DrawMain 
-        titleText={<span>My name is Gabriel.</span>}
-        subtitleText= {<span>I'm a <strong>software engineer</strong> that loves building things to make life better.</span>} 
-        logoAndMore= {
-        <div><img id="avatar_of_me" src={require('./avatar.png')} alt="Avatar of me" />
-            <div className="columns has-text-centered">
-                <div className="column">
-                    <a href="https://github.com/The-Briel-Deal" className="social_icon"><FA name="fab fa-github-square" /></a>
-                    <a href="https://www.linkedin.com/in/gabrielford/" className="social_icon"><FA name="fab fa-linkedin-square" /></a>
-                </div>
-            </div>
-        </div>
-        }
+        titleText={titleText}
+        subtitleText= {subtitleText} 
+        socialBlock = {socialBlock}
+        avatar = {avatar}
         />
-
+        <SectionTwo />
     </div>,
     document.getElementById("root")
 )
