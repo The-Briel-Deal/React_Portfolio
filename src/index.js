3e697cb04506d604fbaba38182c84f52337d7279
Gabriel Ford <gabe@fordltc.net>
2022-01-25 14:58:14 -0500

    removed all jquery

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
-import nav from "./nav";
+import Nav from "./nav";
 import { DrawMain, SectionTwo, Footer, } from "./components";
 import FA from 'react-fontawesome'
 import './index.css'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navigation from './Navigation';
 // CONTENT
 const titleText = <span>My name is Gabriel.</span>
 const subtitleText = <span>I'm a <strong>software engineer</strong> that loves building things to make life better.</span>
@@ -13,9 +14,8 @@ const liLink = <a href="https://www.linkedin.com/in/gabrielford/" className="soc
 const socialBlock = <div className="column">{ghLink}{liLink}</div>
 const avatar = <img id="avatar_of_me" src={require('./avatar.png')} alt="Avatar of me" />
 
-ReactDom.render(
-    <div>
-        {nav}
+const Home = () => {
+    return <div>
         <DrawMain
             titleText={titleText}
             subtitleText={subtitleText}
@@ -24,6 +24,20 @@ ReactDom.render(
         />
         <SectionTwo />
         <Footer />
+    </div>
+}
+ReactDom.render(
+    <div>
+        <Nav />
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    {/* <Route exact path="/" component={Home} /> */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/credentials" element={<Home />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     </div>,
     document.getElementById("root")
 )
--- src/nav.jsx
+++ src/nav.jsx
@@ -1,42 +1,58 @@
 import React from "react";
 import "./index.css";
 
-let nav = (<nav className="navbar is-dark is-transparent is-active" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-        <a className="navbar-item" href="/">
-            <img src="./LogoNoText.png" alt="" />
-        </a>
+let Nav = () => {
+    const [activeBool, setActiveBool] = React.useState(true)
+    const [active, setActive] = React.useState("");
+    return (<nav className="navbar is-dark is-transparent is-active" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+            <a className="navbar-item" href="/">
+                <img src="./LogoNoText.png" alt="" />
+            </a>
 
-        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </a>
-    </div>
+            <button role="button" className={'navbar-burger ' + active} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={
+                () => {
 
-    <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-            <a className="navbar-item" href="credentials">
-                Credentials
-            </a>
-            <a className="navbar-item" href="work_experience">
-                Work Experience
-            </a>
-            <a className="navbar-item" href="projects">
-                Projects
-            </a>
+
+                    if (activeBool) {
+                        setActive("is-active")
+                    }
+                    if (!activeBool) {
+                        setActive("")
+                    }
+                    setActiveBool(!activeBool);
+                }
+            } >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </button>
         </div>
 
-        <div className="navbar-end">
-            <div className="navbar-item">
-                <div className="buttons">
-                    <a href="/contact" className="button is-primary">
-                        Contact Me!
-                    </a>
+        <div id="navbarBasicExample" className={'navbar-menu ' + active}>
+            <div className="navbar-start">
+                <a className="navbar-item" href="credentials">
+                    Credentials
+                </a>
+                <a className="navbar-item" href="work_experience">
+                    Work Experience
+                </a>
+                <a className="navbar-item" href="projects">
+                    Projects
+                </a>
+            </div>
+
+            <div className="navbar-end">
+                <div className="navbar-item">
+                    <div className="buttons">
+                        <a href="/contact" className="button is-primary">
+                            Contact Me!
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</nav>)
+    </nav>)
+}
 
-export default nav
+export default Nav
