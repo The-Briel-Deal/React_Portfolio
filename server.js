890454afe835aff11f98b5886281e6ed72a2207b
Gabriel Ford <gabe@fordltc.net>
2022-01-26 11:28:17 -0800

    saving yarn changes before I pull

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(9000);
-=======
-const express = require('express')
-
-const app = express()
-const port = 3000
-
-app.use(express.static('build'));
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
->>>>>>> 5dfd15db985c24c4a3f747af823672de9c75f368
