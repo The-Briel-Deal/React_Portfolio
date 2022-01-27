fd8effdf8ac60b734655505f93da8f50f473d7c9
Gabriel Ford <gabe@fordltc.net>
2022-01-27 08:03:17 -0500

    installed nodemon

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
