ce2ec3c98362a671a29b1fa5af78c2e00193fc15
Gabriel Ford <gabe@fordltc.net>
2022-01-27 10:27:10 -0500

    fixed animation and favicon

--- src/animations/AnimatedPage.jsx
+++ src/animations/AnimatedPage.jsx
@@ -1,20 +1,20 @@
 import { motion } from "framer-motion";
 
 const animations = {
-    initial: { opacity: 1, y:0, scale: 1.5},
-    animate: { opacity: 1, y:0, scale: 1 },
-    exit: { opacity: 1, y: 0, scale: 1.5},
+    initial: { opacity: 1, y: 500, scale: .5 },
+    animate: { opacity: 1, y: 0, scale: 1 },
+    exit: { opacity: 1, y: 0, scale: 1.5 },
 };
 
 const AnimatedPage = ({ children }) => {
     return (
         <motion.div
-        variants={animations}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        transition={{duration: 1}}
-        
+            variants={animations}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{ duration: .7 }}
+
         >
             {children}
         </motion.div>
