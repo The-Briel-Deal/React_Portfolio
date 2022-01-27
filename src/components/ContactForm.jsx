a5aed46ecda5afe6424ff3b88745c98bb90a8947
Gabriel Ford <gabe@fordltc.net>
2022-01-27 08:01:32 -0500

    working on form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,29 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+    const [form, setForm] = useState({
+        subject: "",
+        name: "",
+        message: "",
+    })
+    const onFormSubmit = () => {
+        console.log({ form })
+    }
+
+    const formChanged = (event) => {
+        console.log(event.target.value)
+        setForm((prev) => {
+            if (event.target.id === "Subject") {
+                prev.subject = event.target.value;
+            }
+            if (event.target.id === "Name") {
+                prev.name = event.target.value;
+            }
+            if (event.target.id === "Message") {
+                prev.message = event.target.value;
+            }
+            return { subject: prev.subject, message: prev.message, name: prev.name }
+        })
+
+    }
+
     return <div className="container has-text-centered">
         <br />
+        <h1>{form.name && <span>Thanks for reaching out {form.name}!</span>}</h1>
         <div className="columns">
             <div className="column is-one-sixth" />
             <div className="column is-one-third">
-                <input style={{ width: "80%" }} id="Message" placeholder="Subject" className="input" />
+                <label className="label">Subject:</label>
+                <input
+                    style={{ width: "80%" }}
+                    id="Subject"
+                    value={form.subject}
+                    onChange={formChanged}
+                    placeholder="Subject"
+                    className="input" />
             </div>
             <div className="column is-one-third">
-                <input style={{ width: "80%" }} id="Message" placeholder="Name" className="input" />
+                <label className="label">Name:</label>
+                <input
+                    style={{ width: "80%" }}
+                    id="Name"
+                    placeholder="Name"
+                    className="input"
+                    value={form.name}
+                    onChange={formChanged} />
             </div>
             <div className="column is-one-sixth" />
         </div>
         <div className="columns">
             <div className="column is-one-sixth" />
             <div className="column is-two-thirds">
-                <textarea style={{ width: "90%", height: "10rem" }} id="Message" placeholder="Message" className="input" />
+                <label className="label">Message:</label>
+                <textarea
+                    style={{ width: "90%", height: "10rem" }}
+                    id="Message"
+                    placeholder="Message"
+                    className="input"
+                    value={form.message}
+                    onChange={formChanged} />
             </div>
             <div className="column is-one-sixth" />
         </div>
         <div className="columns">
             <div className="column is-one-sixth" />
             <div className="column is-two-thirds">
-                <button class="button is-success">Send</button>
+                <button
+                    onClick={onFormSubmit}
+                    className="button is-success">Send</button>
             </div>
             <div className="column is-one-sixth" />
         </div>
